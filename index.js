815df1f73f984973ba6ff583997c1a023b63395d
nikolasmelui <slak@samaradom.ru>
2020-01-30 16:00:18 +0400

    Add index.js file with main server creation logic

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const http = require('http');
-
 const { server } = require('./config');
+const handler = require('./handler');
 
 http
-  .createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Server is working');
-  })
+  .createServer(handler)
   .listen(server.port, server.hostname, () =>
-    console.log(`Server running at http://${server.hostname}:${server.port}`),
+    console.info(`Server running at http://${server.hostname}:${server.port}`),
   );
